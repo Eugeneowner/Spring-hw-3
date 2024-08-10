3d944557f619e2f6cb9c769758035f707d743056
Eugeneowner <ownereugene@gmail.com>
2024-08-10 15:56:34 +0300

    done

--- src/main/resources/sql/data-postgres.sql
+++ src/main/resources/sql/data-postgres.sql
@@ -1,7 +1,7 @@
 INSERT INTO customers (name, email, age, password, phone, created_date, last_modified_date)
 VALUES ('Eugene', 'eugene@gmail.com', 35, 'qWerty', '+1234567890', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
        ('Tanya', 'Tanya@gmail.com', 34, 'qWerty', '+0987654321', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-       ('Den', 'den@gmail.com', 35, 'qWerty', '+1122334455', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       ('Den', 'den@gmail.com', 35, 'qWerty', '+1122334455', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 
 INSERT INTO accounts (number, currency, balance, customer_id, created_date, last_modified_date)
